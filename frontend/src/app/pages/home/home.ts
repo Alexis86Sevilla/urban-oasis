b25a085b2922f0803783e43c08f2d8b49c3311b5
Alexis86Sevilla <alegarman86@gmail.com>
2026-06-15 21:02:38 +0200

    feat(home): add filter buttons with active state and responsive layout

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import * as L from 'leaflet';
+import { ChangeDetectionStrategy, Component, computed, effect, inject, OnChanges, SimpleChanges } from '@angular/core';
 import { MapView } from "../../components/map-view/map-view";
+import { OasisService } from '../../services/oasis';
+import { WeatherService } from '../../services/wheater';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +9,30 @@ import { MapView } from "../../components/map-view/map-view";
   templateUrl: './home.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Home {}
+export class Home {
+  private weatherService = inject(WeatherService);
+  protected readonly oasisService = inject(OasisService);
+  protected readonly wheater = this.weatherService.weather;
+  protected readonly temperatureIndicator = computed(() => {
+    const temp = this.wheater()?.temperature;
+    if (temp! < 30) {
+      return 'bg-green-100 text-green-700';
+    } else if (temp! >= 30 && temp! < 35) {
+      return 'bg-orange-100 text-orange-700';
+    } else {
+      return 'bg-red-100 text-red-700';
+    }
+  });
+
+  constructor() {
+    this.weatherService.getWeather();
+  }
+
+  filter(type: string) {
+    this.oasisService.activeFilter.set(type);
+  }
+
+  updateActualPosition() {
+    this.oasisService.updateActualPosition();
+  }
+}
